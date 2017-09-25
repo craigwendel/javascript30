c1d95cd83da2a0fb063f3d1ce3695a3a00aacdfe
Craig Wendel <wendelcraig@gmail.com>
2017-09-25 16:46:23 -0500

    Add logic for hour hand to translate correcty. Add logic to not transition on 60 second tick.

--- js-clock/main.js
+++ js-clock/main.js
@@ -1,17 +1,24 @@
 const secondHand = document.querySelector('.second-hand')
 const minuteHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 
 function setDate () {
   const now = new Date()
   const seconds = now.getSeconds()
-  const secondsDegrees = (((seconds / 60) * 360) + 90)
+  const secondsDegrees = ((seconds / 60) * 360) + 90
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
+  if (seconds === 0) {
+    secondHand.style.transition = 'none'
+  }
+
   const minutes = now.getMinutes()
-  const minutesDegrees = (((minutes / 60) * 360) + 90)
+  const minutesDegrees = ((minutes / 60) * 360) + 90
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
 
-  const hours = 
+  const hour = now.getHours()
+  const hourDegrees = ((hour / 12) * 360) + 90
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`
 }
 
 setInterval(setDate, 1000)
