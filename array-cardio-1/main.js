baa77aa392beab587e3e2a2771cad0dfe65a7784
Craig Wendel <wendelcraig@gmail.com>
2017-10-12 21:27:36 -0500

    Add reduce and sort array methods to remaining 2 problems.

--- array-cardio-1/main.js
+++ array-cardio-1/main.js
@@ -114,7 +114,14 @@ console.table(oldest)
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-// TODO: Need to complete
+// Code for Wikipedia Page
+
+// const category = document.querySelector('.mw-category')
+// const links = Array.from(category.querySelectorAll('a'))
+//
+// const de = links
+//             .map(link => link.textContent)
+//             .filter(streetName => streetName.includes('de'))
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
@@ -130,21 +137,14 @@ console.log(lastNameAlpha)
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 
-// TODO: Need to complete
+const data = [ 'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'scooter', 'run', 'run', 'jog' ]
 
-const data = [
-  'car',
-  'car',
-  'truck',
-  'truck',
-  'bike',
-  'walk',
-  'car',
-  'van',
-  'bike',
-  'walk',
-  'car',
-  'van',
-  'car',
-  'truck'
-]
+const transport = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0
+  }
+  obj[item]++
+  return obj
+}, {})
+
+console.log(transport)
